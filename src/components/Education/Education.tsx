27260449b3a83322a9061395ae9c1bcdb4483b20
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2025-10-19 12:27:50 -0300

    Add 2 certificates

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -70,15 +70,15 @@ export const Education = () => {
         <article className="edu__certificates--mobile">
 
             <div className="edu__certificate--mobile">
-              <h4>CS50 Artificial Intelligence</h4>
-              <a href="https://certificates.cs50.io/e7bf8cba-c7d7-4a4f-bc87-a87c23cda397.pdf?size=letter" target="_blank" rel="noopener noreferrer">Link of certificate</a>
-              <img loading="lazy" src="https://i.postimg.cc/L4C3Vvqd/CS50-AI-min.png" alt="CS50 Harvard Artificial Intelligence Course Certificate" />
+              <h4>CS50 Databases with SQL</h4>
+              <a href="https://certificates.cs50.io/592e207d-fc25-4497-9073-4aa12aeca9c6.pdf?size=letter" target="_blank" rel="noopener noreferrer">Link of certificate</a>
+              <img loading="lazy" src="https://i.postimg.cc/8zWqVX8v/CS50-SQL.png" alt="CS50 Harvard Databases with SQL Course Certificate" />
             </div>
 
             <div className="edu__certificate--mobile">
-              <h4>CS50 Computer Science Harvard Certificate</h4>
-              <a href="https://certificates.cs50.io/7bb466dd-c10b-4769-bf63-4c885013b122.pdf?size=letter" target="_blank" rel="noopener noreferrer">Link of certificate</a>
-              <img loading="lazy" src="https://i.postimg.cc/50bVWhMr/CS50x-min.png" alt="CS50 Harvard Computer Science Course Certificate" />
+              <h4>CS50 Artificial Intelligence</h4>
+              <a href="https://certificates.cs50.io/e7bf8cba-c7d7-4a4f-bc87-a87c23cda397.pdf?size=letter" target="_blank" rel="noopener noreferrer">Link of certificate</a>
+              <img loading="lazy" src="https://i.postimg.cc/L4C3Vvqd/CS50-AI-min.png" alt="CS50 Harvard Artificial Intelligence Course Certificate" />
             </div>
 
             <div className="edu__certificate--mobile">
@@ -91,6 +91,18 @@ export const Education = () => {
               <h4>CSS Master Udemy Certificate</h4>
               <a href="https://www.udemy.com/certificate/UC-412cea72-9b35-427b-8019-cda1f36dc946/" target="_blank" rel="noopener noreferrer">Link of certificate</a>
               <img loading="lazy" src="https://i.postimg.cc/HxBfDjzJ/UC-412cea72-9b35-427b-8019-cda1f36dc946-min.jpg" alt="CSS Master Course Certificate" />
+            </div>
+
+            <div className="edu__certificate--mobile">
+              <h4>CS50 Computer Science Harvard Certificate</h4>
+              <a href="https://certificates.cs50.io/7bb466dd-c10b-4769-bf63-4c885013b122.pdf?size=letter" target="_blank" rel="noopener noreferrer">Link of certificate</a>
+              <img loading="lazy" src="https://i.postimg.cc/50bVWhMr/CS50x-min.png" alt="CS50 Harvard Computer Science Course Certificate" />
+            </div>
+
+            <div className="edu__certificate--mobile">
+              <h4>Scikit-Learn for Beginners Simplilearn Certificate</h4>
+              <a href="https://certificates.simplicdn.net/share/9019235_93951051758414971759.pdf" target="_blank" rel="noopener noreferrer">Link of certificate</a>
+              <img loading="lazy" src="https://i.postimg.cc/dtgzpPf2/9019235-93951051758414970139.png" alt="Simplilearn Scikit-Learn for Beginners Course Certificate" />
             </div>
 
             <div className="edu__certificate--mobile">
