a0a31abe7b6fa18953fe0559d4ad4a9748e91cff
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2025-10-22 20:57:42 -0300

    Change Header background

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,17 +10,18 @@ gsap.registerPlugin(useGSAP, ScrollTrigger)
 export const Header = () => {
 
   useGSAP(() => {
-    gsap.from(".wave", {
-      scrollTrigger: {
-        trigger: ".header-landscape",
-        // toggleActions: "play pause restart none",
-        toggleActions: "play none none none",
-      },
-      // opacity: 0,
-      scale: 2,
-      duration: 1,
-      ease: "power3.out"
-    })
+    // Uncomment this line, and all dependencies of to enable wave animation
+    // gsap.from(".wave", {
+    //   scrollTrigger: {
+    //     trigger: ".header-landscape",
+    //     // toggleActions: "play pause restart none",
+    //     toggleActions: "play none none none",
+    //   },
+    //   // opacity: 0,
+    //   scale: 2,
+    //   duration: 1,
+    //   ease: "power3.out"
+    // })
 
     gsap.from([".header__title",".header__subtitle",".header__contacts"], {
       scrollTrigger: {
@@ -30,7 +31,7 @@ export const Header = () => {
       },
       opacity: 0,
       // scale: 1.6,
-      duration: .7,
+      duration: .2,
       ease: "power3.in",
       stagger: .1
     })
@@ -40,8 +41,9 @@ export const Header = () => {
     <>
       <section className="header-landscape">
     
-        <div className="wave"></div>
-
+        {/* <div className="wave"></div> */}
+        
+        <svg className="svg" xmlns='http://www.w3.org/2000/svg' width='100%' height='100%' ><rect fill='#ffffff' width='24' height='24'/><defs><linearGradient id='a' x1='0' x2='0' y1='0' y2='1'><stop offset='0'  stop-color='#a00'/><stop offset='1'  stop-color='#111'/></linearGradient></defs><pattern id='b'  width='42' height='42' patternUnits='userSpaceOnUse'><circle  fill='#ffffff' cx='21' cy='21' r='21'/></pattern><rect width='100%' height='100%' fill='url(#a)'/><rect width='100%' height='100%' fill='url(#b)' fill-opacity='0.1'/></svg>
 
         <article className='header__container'>
 
