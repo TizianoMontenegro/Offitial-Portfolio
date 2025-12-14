fa82bfe67e8eb511a510e4df4a5cddfc9cf4dd67
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2025-12-14 14:01:16 -0300

    Redesign education section. Made responsive

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -13,9 +13,16 @@ export const Education = () => {
           {certificatesData.map((certificate, index) => {
             return (
               <div key={index} className="edu__certificate--mobile">
-                <h4>{certificate.title}</h4>
-                <a href={certificate.documentUrl} target="_blank" rel="noopener noreferrer">Link of certificate</a>
-                <img loading="lazy" src={certificate.imageUrl} alt={certificate.imageAlt} />
+                {/* <h4>{certificate.title}</h4> */}
+                {/* <a className="edu__certificate--verify" href={certificate.documentUrl} target="_blank" rel="noopener noreferrer"> */}
+                  <img loading="lazy" src={certificate.imageUrl} alt={certificate.imageAlt} />
+                {/* </a> */}
+                <a className="edu__certificate--verify--mobile" href={certificate.documentUrl} target="_blank" rel="noopener noreferrer">Click Here to Verify</a>
+                <div className="edu__certificate--banner">
+                  <h4>{certificate.title}</h4>
+                  <p>{certificate.description}</p>
+                  {/* <p>{certificate.description}</p> */}
+                </div>
               </div>
             )
           })}
