f87ef54ca0d8b43a870422a435f9ee1187e5285e
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2025-12-14 16:34:36 -0300

    Clean unnecessary lines and comments

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -8,20 +8,16 @@ export const Education = () => {
       <section className="edu">
         <h2 className="edu__title">Certifications</h2>
 
-        <article className="edu__certificates--mobile">
+        <article className="edu__certificates">
 
           {certificatesData.map((certificate, index) => {
             return (
-              <div key={index} className="edu__certificate--mobile">
-                {/* <h4>{certificate.title}</h4> */}
-                {/* <a className="edu__certificate--verify" href={certificate.documentUrl} target="_blank" rel="noopener noreferrer"> */}
-                  <img loading="lazy" src={certificate.imageUrl} alt={certificate.imageAlt} />
-                {/* </a> */}
-                <a className="edu__certificate--verify--mobile" href={certificate.documentUrl} target="_blank" rel="noopener noreferrer">Click Here to Verify</a>
+              <div key={index} className="edu__certificate">
+                <img loading="lazy" src={certificate.imageUrl} alt={certificate.imageAlt} />
+                <a className="edu__certificate--verify" href={certificate.documentUrl} target="_blank" rel="noopener noreferrer">Click Here to Verify</a>
                 <div className="edu__certificate--banner">
                   <h4>{certificate.title}</h4>
                   <p>{certificate.description}</p>
-                  {/* <p>{certificate.description}</p> */}
                 </div>
               </div>
             )
