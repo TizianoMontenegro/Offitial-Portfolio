c5b06f50244746841ca669db328a9279286b31f6
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2025-12-15 10:49:41 -0300

    Add divisor between education section

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -40,7 +40,16 @@ export const Main = () => {
 
         {/* <div className="divisor" /> */}
 
+
       </section>
+
+      {/* DIVISOR  */}
+      <div className="custom-shape-divider-top-1765805787">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M1200 0L0 0 598.97 114.72 1200 0z" className="shape-fill"></path>
+        </svg>
+      </div>
+      
     </main>
   )
 }
