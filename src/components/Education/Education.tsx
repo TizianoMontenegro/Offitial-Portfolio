bcb640f01015f55b8849686a1b8865702e544c20
Tiziano Montenegro <tizianomontenegro07@gmail.com>
2025-12-15 10:43:57 -0300

    Add techs in banner. Add divisor between projects

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -5,6 +5,13 @@ export const Education = () => {
 
   return (
     <div id="education-page" className="edu-wrapper">
+      
+      <div className="custom-shape-divider-top-1765804317">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path d="M1200 0L0 0 598.97 114.72 1200 0z" className="shape-fill"></path>
+        </svg>
+      </div>
+
       <section className="edu">
         <h2 className="edu__title">Certifications</h2>
 
@@ -18,6 +25,13 @@ export const Education = () => {
                 <div className="edu__certificate--banner">
                   <h4>{certificate.title}</h4>
                   <p>{certificate.description}</p>
+                  <ul className="edu_certificate--techs">
+                    {certificate.techs.map((tech, tech_i) => {
+                      return (<li key={tech_i} className="edu_certificate--tech">
+                        {tech}
+                      </li>)
+                    })}
+                  </ul>
                 </div>
               </div>
             )
